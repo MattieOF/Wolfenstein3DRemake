3c233b8271c79c96623c789c4c31d28e429eff51
Mattie <mattbware1@gmail.com>
2020-11-17 21:35:14 +0000

    Start level system

---
+++ Assets/Scripts/Level/LevelData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+[Serializable]
+public class LevelData
+{
+    public string name;
+    public TileInfo[,] tiles;
+
+    public LevelData(int xSize, int ySize)
+    {
+        tiles = new TileInfo[xSize, ySize];
+    }
+
+    public TileInfo GetTileAt(int x, int y)
+    {
+        return tiles[x, y];
+    }
+
+    public bool IsTileAt(int x, int y)
+    {
+        return tiles[x, y] != null;
+    }
+}
---
+++ Assets/Scripts/Level/TileInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "New Tile", menuName = "Game/Tile")]
+public class TileInfo : ScriptableObject
+{
+    public Texture texture;
+
+    [HideInInspector]
+    public bool moveableTile = false;
+    [HideInInspector]
+    public Vector2 tileMoveOffset = new Vector2();
+
+    // TODO Possible support for directional textures?
+}
