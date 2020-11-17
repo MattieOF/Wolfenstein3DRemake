1dcbda009b4eb49d81e0a5d7e05003d51dd7510b
Mattie <mattbware1@gmail.com>
2020-11-17 03:21:55 +0000

    Basic Editor and Editor Camera

---
+++ Assets/Scripts/EditorCameraControl.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public enum EditorCameraProjection
+{
+    Perspective,
+    Orthrographic
+}
+
+public class EditorCameraControl : MonoBehaviour
+{
+    [Header("Camera Properties")]
+    public float zoomAmount = 3f;
+    public float moveSpeed = 2f;
+    public bool acceptInput = true;
+    public EditorCameraProjection projection = EditorCameraProjection.Perspective;
+
+    [Header("Keybinds")]
+    public KeyCode toggleProjection = KeyCode.P;
+
+    private float mouseWheel;
+    private float horizontal;
+    private float vertical;
+
+    void Start()
+    {
+        if (projection == EditorCameraProjection.Orthrographic) GetComponent<Camera>().orthographic = true;
+        else GetComponent<Camera>().orthographic = false;
+    }
+
+    // Input polling 
+    void Update()
+    {
+        if (!acceptInput) return;
+
+        mouseWheel = Input.mouseScrollDelta.y;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(toggleProjection))
+        {
+            if (projection == EditorCameraProjection.Orthrographic)
+            {
+                projection = EditorCameraProjection.Perspective;
+                GetComponent<Camera>().orthographic = false;
+            } else
+            {
+                projection = EditorCameraProjection.Orthrographic;
+                GetComponent<Camera>().orthographic = true;
+            }
+        }
+    }
+
+    // Perform camera movement here
+    void FixedUpdate()
+    {
+        if (mouseWheel != 0)
+        {
+            if (projection == EditorCameraProjection.Orthrographic)
+            {
+                GetComponent<Camera>().orthographicSize += zoomAmount * -mouseWheel * 0.5f;
+            }
+            else
+            {
+                Vector3 pos = transform.position;
+                pos.y += zoomAmount * -mouseWheel;
+                transform.position = pos;
+            }
+        }
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            Vector3 pos = transform.position;
+            pos.x += horizontal * moveSpeed;
+            pos.z += vertical * moveSpeed;
+            transform.position = pos;
+        }
+    }
+
+    public void ResetInput()
+    {
+        vertical = 0;
+        horizontal = 0;
+        mouseWheel = 0;
+    }
+}
