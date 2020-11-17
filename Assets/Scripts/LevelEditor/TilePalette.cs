d594b235d34870a959797bcde9bac9a25d6b3272
Mattie <mattbware1@gmail.com>
2020-11-17 22:07:17 +0000

    Tile palette loads tiles from tiles folder

--- Assets/Scripts/LevelEditor/TilePalette.cs
+++ Assets/Scripts/LevelEditor/TilePalette.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TilePalette : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Scene References")]
+    public Transform contentParent;
+
+    [Header("Asset References")]
+    public GameObject itemPrefab;
+    
+    private TileInfo[] tiles;
+
     void Start()
     {
-        
+        LoadTiles();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadTiles()
     {
-        
+        tiles = Resources.LoadAll<TileInfo>("Tiles");
+
+        foreach (TileInfo tile in tiles)
+        {
+            GameObject go = Instantiate(itemPrefab, contentParent);
+            go.GetComponent<RawImage>().texture = tile.texture;
+        }
     }
 }
